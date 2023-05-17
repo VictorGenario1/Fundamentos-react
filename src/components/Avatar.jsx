20a2fc7ea2bd166ed69eb17ca375560e6b64e60f
VictorGenario1 <victordamatagenario2@gmail.com>
2023-05-17 06:48:57 -0300

    componente avatar

--- src/components/Avatar.jsx
+++ src/components/Avatar.jsx
@@ -1,9 +1,10 @@
 import styles from "./Avatar.module.css"
 
-export function Avatar(){
+export function Avatar({hasBorder = true , src}){
     return(
         <img 
-            className={styles.avatar}
+            className={hasBorder ? styles.avatarWithBorder : styles.avatar}
+            src={src}
         />
     );
 }

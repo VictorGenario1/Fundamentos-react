1d3e6ef6e0c343fc9c75c5068087c4ac7e5fdd7a
VictorGenario1 <victordamatagenario2@gmail.com>
2023-05-19 06:57:17 -0300

    Interando posts

--- src/App.jsx
+++ src/App.jsx
@@ -2,31 +2,64 @@ import { Header } from "./components/Header"
 import { Sidebar } from "./components/Sidebar"
 import { Post } from "./components/Post"
 
-
 import styles from './App.module.css'
 import './global.css'
+import { Key } from "phosphor-react"
+
+const posts = [
+{
+id: 1,
+author: {
+avatarUrl: "https://github.com/victorGenario.png",
+name: "victor genario",
+role: "Student @Rocketseat"
+},
+content: [
+{ type: 'paragraph', content: "Fala galeraa 👋"},
+{ type: 'paragraph', content: "Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀"},
+{ type: 'link', content: "👉 jane.design/doctorcare"}
+],
+publishedAt: new Date('2023-05-18 06:57:00'),
+},
+{
+id: 2,
+author: {
+avatarUrl: "https://github.com/victorGenario.png",
+name: "victor genario",
+role: "Student @Rocketseat"
+},
+content: [
+{ type: 'paragraph', content: "Fala galeraa 👋"},
+{ type: 'paragraph', content: "Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀"},
+{ type: 'link', content: "👉 jane.design/doctorcare"}
+],
+publishedAt: new Date('2023-05-18 06:57:00'),
+}
+]
+
+export function App() {
+return (
+<>
+<Header />
+<div className={styles.wrapper}>
+<Sidebar />
+
+<main>
+{posts.map(post=>  {
+return(
+<Post 
+author={post.author}
+content={post.content}
+publishedAt={post.publishedAt}
+/>
+)
+})}
+</main> 
 
 
-
-function App() {
-    return (
-    <>
-      <Header />
-    
-      <div className={styles.wrapper}>
-        <Sidebar />
-
-        <main>
-         <Post />
-         <Post />
-        </main>
-
-
-
-      </div>
-      
-    </>
-  )
+</div>
+</>
+)
 }
 
-export default App
+

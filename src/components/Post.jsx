845fca7324e4fa15e7bc00d6bf730b56e0048c95
VictorGenario1 <victordamatagenario2@gmail.com>
2023-05-20 06:17:34 -0300

    Adicionando comentarios usando o useState

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -4,8 +4,17 @@ import { ptBR } from 'date-fns/locale'
 import { Avatar } from './Avatar'
 import { Comment } from './Comment'
 import styles from './Post.module.css'
+import { useState } from 'react'
+
+
 
 export function Post({ author, publishedAt, content }){
+    const [comments, setComments] = useState([
+        1,
+        2
+    ])
+    
+    
     const publishedDateFormatted = format(publishedAt, "d 'de' LLLL 'às' HH:mm 'h' ", {
         locale: ptBR,    
     })
@@ -14,6 +23,12 @@ export function Post({ author, publishedAt, content }){
         locale: ptBR,
         addSuffix: true,
     })
+
+    function handleCreateNewComment(){
+        event.preventDefault()
+
+        setComments([...comments,comments.length + 1]);
+    }
 
     return(
         <article className={styles.post}>
@@ -41,7 +56,7 @@ export function Post({ author, publishedAt, content }){
                 })}
             </div>
 
-            <form className={styles.commentForm}>
+            <form onSubmit={handleCreateNewComment} className={styles.commentForm}>
                 <strong>Deixe seu feedback</strong>
 
                 <textarea 
@@ -53,9 +68,9 @@ export function Post({ author, publishedAt, content }){
             </form>
 
             <div className={styles.commentList}>
-                <Comment />
-                <Comment />
-                <Comment />
+                {comments.map(comment =>{
+                    return <Comment />
+                })}
             </div>
 
         </article>
